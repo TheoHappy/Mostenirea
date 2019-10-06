9bc8c84154e92d36472b73b14734ac2cada99b7d
Theo <teodore473@gmail.com>
2019-10-06 13:12:16 +0300

    Adaugare clasei HardDisks si Computers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    Product ceas = new Product ("CasioG3", "Rol", 178.6f,false,123);
-	    System.out.println(ceas.toString());
+	    Computers lenovoThinkpad = new Computers("Lenovo Thinkpad","Lenova",699.99f,true,87, "i7-9750H","MX-150","Intel Cannon Lake Z390 ",16, 1024);
+	    System.out.println(lenovoThinkpad.toString());
+	    HardDisks seagateBarracudaST500DM002 = new HardDisks("ST500DM002", "Seagate",50, false, 15, "3.5 Inch Pentru PC", 500, 7200, 16);
+	    System.out.println(seagateBarracudaST500DM002.toString());
     }
 }
